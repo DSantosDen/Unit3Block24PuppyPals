7b2bf2f3af7eb8b4167650bd53aa62f01d966534
DSantosDen <51485458+DSantosDen@users.noreply.github.com>
2024-05-08 19:01:22 -0600

    Learning how to use React via CDN

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { puppyList } from "./data.js";
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppyList);
   return (
-    <>
-      <div>
-
-      </div>
-    </>
-  )
+    <div>
+      {puppies.map((puppy) => {
+        return <p>{puppy.name}</p>;
+      })}
+    </div>
+  );
 }
 
-export default App
+export default App;
